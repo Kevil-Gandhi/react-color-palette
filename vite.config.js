c2f79898e7aa3ca4ccfedc248470b948a1de927f
Kevil-Gandhi <kevilmgandhi@gmail.com>
2025-09-21 16:26:31 +0530

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-color-palette/",
+  base: "/react-color-palette",
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
